f60a75e08effa7b74e1d4996e1bd1a8a9ca8e98a
ian10k <kaleb.dominguez2010@gmail.com>
2026-05-04 15:20:43 -0600

    Add files via upload

---
+++ #Promedio.py ian.py
@@ -0,0 +1,8 @@
+#Promedio
+#ian dominguez
+numeros = [65, 32, 17, 76, 3, 46, 90, 23, 67, 1, 34]
+
+total = sum(numeros)
+promedio = sum(numeros)  / len(numeros)
+print(f'suma:{total}')
+print(f'Promedio: {promedio:.2f}')
