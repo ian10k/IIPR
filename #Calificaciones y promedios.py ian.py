059db84541b1841b472fa301b234acfadc2049aa
ian10k <kaleb.dominguez2010@gmail.com>
2026-05-04 15:15:13 -0600

    Add files via upload

---
+++ #Calificaciones y promedios.py ian.py
@@ -0,0 +1,18 @@
+#Calificaciones y promedios
+#IAN DOMINGUEZ
+
+cal1 = 10
+cal2 = 9
+cal3 = 8
+cal4 = 10
+cal5 = 7
+
+#Calculamos promedio
+promedio = (cal1 + cal2 + cal3 + cal4 + cal5) / 5
+print("Las calificaciones son:", cal1, cal2, cal3, cal4, cal5)
+print("El promedio es:", promedio)
+      
+
+
+
+
