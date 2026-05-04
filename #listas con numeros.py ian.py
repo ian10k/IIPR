933a1797297e765d4d6e3ba154d482ea8cf89b32
ian10k <kaleb.dominguez2010@gmail.com>
2026-05-04 15:19:35 -0600

    Add files via upload

---
+++ #listas con numeros.py ian.py
@@ -0,0 +1,13 @@
+#lista con numeros
+#ian dominguez
+numeros = [21, 12, 13, 98, 3, 76, 90, 23, 67, 1, 64]
+print('Original:',numeros)
+
+#Ordenar de menor a mayor
+numeros.sort()
+print('Ordenada:' ,numeros)
+
+#Ordenar de mayor a menor
+
+numero.reverse()
+print('Invertida:', numeros)
