a29308e1695b36242bc005c7968edefc7cfa1dd1
ian10k <kaleb.dominguez2010@gmail.com>
2026-05-07 20:41:59 -0600

    Add files via upload

---
+++ contador_pares = 0.py
@@ -0,0 +1,9 @@
+contador_pares = 0
+
+for numero in range(1, 51):
+
+    if numero % 2 == 0:
+
+        contador_pares += 1
+
+print(f"La cantidad de números pares entre 1 y 50 es: {contador_pares}")
---
+++ factorial.py
@@ -0,0 +1,2 @@
+factorial = 6 * 5 * 4 * 3 * 2 * 1
+print(f"el factorial de 6 es" , factorial)
